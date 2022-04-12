ee332caaa8b6b3faf224a8bb8aeef2e8667ed653
Thomas Cintra <tcintra@g.hmc.edu>
2022-04-12 15:16:20 -0700

    Preliminary runs of RF model on core data, with and without VOCs/Emissions

--- generate.py
+++ generate.py
@@ -21,9 +21,9 @@ STATE   = '06'
 datafetcher = DataFetcher()
 
 # CRITERIA and MET variables
-# core_df = datafetcher.create_dataset(20180101, 20181231, site=SITE, county=COUNTY, state=STATE, processed=True, verbose=False, vocs=False)
-# core_df.to_csv(PATH+'core.csv')
-# print("Finished core df.")
+core_df = datafetcher.create_dataset(20180101, 20181231, site=SITE, county=COUNTY, state=STATE, processed=True, verbose=False, vocs=False)
+core_df.to_csv(PATH+'core.csv')
+print("Finished core df.")
 
 # AQS VOCS and corresponding emisions
 final_vocs, final_emissions = datafetcher.get_final_compounds()
@@ -31,9 +31,9 @@ final_vocs, final_emissions = datafetcher.get_final_compounds()
 # NOTE: REMOVE m/p Xylene data as its indices are scrambled and so I assume the data is untrustworthy.
 final_vocs.remove('m/p Xylene')
 
-# vocs_df = datafetcher.get_voc_data(20180101, 20181231, STATE, COUNTY, SITE, final_vocs)
-# vocs_df.to_csv(PATH+'vocs.csv')
-# print("Finished vocs df.")
+vocs_df = datafetcher.get_voc_data(20180101, 20181231, STATE, COUNTY, SITE, final_vocs)
+vocs_df.to_csv(PATH+'vocs.csv')
+print("Finished vocs df.")
 
 emissions_df = datafetcher.get_ceds_data('2018', keep=final_emissions) 
 emissions_df.to_csv(PATH+'emissions.csv')
